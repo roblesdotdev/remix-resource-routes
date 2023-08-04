2167747d5dd69afa66699e4e84e217b0dad8a181
roblesdotdev <httpsnip@gmail.com>
2023-08-03 22:46:31 -0300

    fix netlify deploy

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
       ? './server.ts'
       : undefined,
-  serverBuildPath: '.netlify/functions-internal/server.js',
+  serverBuildPath: '.netlify/functions/server.js',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
