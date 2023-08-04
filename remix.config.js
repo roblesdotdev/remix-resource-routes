258de9dbad46d59f7a692359bb6b1c622e2f59a2
roblesdotdev <httpsnip@gmail.com>
2023-08-03 22:31:57 -0300

    netlify server

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? './server.ts'
+      : undefined,
+  serverBuildPath: '.netlify/functions-internal/server.js',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
